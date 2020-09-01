03fb6837c167ca06096df0c83c90826a58d11bbb
rishabh <rishabhc329@gmail.com>
2020-09-01 16:16:57 +0530

    Updates

---
+++ precache-manifest.f2df47cc0fa1feef59cb6fe7183284fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ad59f2716c3b60c4176faa46885b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "703d9469605fd3aa1b49",
+    "url": "/static/css/main.f471dea3.chunk.css"
+  },
+  {
+    "revision": "1a858f0418e596522bf1",
+    "url": "/static/js/2.5defb1e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5defb1e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703d9469605fd3aa1b49",
+    "url": "/static/js/main.eca12232.chunk.js"
+  },
+  {
+    "revision": "61c5fc03092afc2952f3",
+    "url": "/static/js/runtime-main.88cd32a5.js"
+  },
+  {
+    "revision": "7da6f7a2623bd10a115cd15fc30b69e6",
+    "url": "/static/media/list.7da6f7a2.svg"
+  },
+  {
+    "revision": "5437e5214c32dc0a8b1e3e3749edd831",
+    "url": "/static/media/search.5437e521.svg"
+  }
+]);

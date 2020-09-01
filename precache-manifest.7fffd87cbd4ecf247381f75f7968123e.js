f35cb9666868fbbd20340eaefc6b4c0435906a07
rishabh <rishabhc329@gmail.com>
2020-09-01 16:21:58 +0530

    Updates

---
+++ precache-manifest.7fffd87cbd4ecf247381f75f7968123e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833a7b976eabba5b6d21d26ea203e19c",
+    "url": "/TopRamen/index.html"
+  },
+  {
+    "revision": "6921efc1f6de803e6047",
+    "url": "/TopRamen/static/css/main.f471dea3.chunk.css"
+  },
+  {
+    "revision": "6d0cc3e5ee65520fd366",
+    "url": "/TopRamen/static/js/2.abc560ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TopRamen/static/js/2.abc560ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6921efc1f6de803e6047",
+    "url": "/TopRamen/static/js/main.d7272f8d.chunk.js"
+  },
+  {
+    "revision": "464b11951e45848757bc",
+    "url": "/TopRamen/static/js/runtime-main.dfc756ec.js"
+  },
+  {
+    "revision": "7da6f7a2623bd10a115cd15fc30b69e6",
+    "url": "/TopRamen/static/media/list.7da6f7a2.svg"
+  },
+  {
+    "revision": "5437e5214c32dc0a8b1e3e3749edd831",
+    "url": "/TopRamen/static/media/search.5437e521.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2df47cc0fa1feef59cb6fe7183284fb.js"
+  "/TopRamen/precache-manifest.7fffd87cbd4ecf247381f75f7968123e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TopRamen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
